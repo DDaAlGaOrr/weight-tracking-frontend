ef444fdac4a77b7b8a8d48a8b0c88dfb49ec29df
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2022-07-20 19:31:01 -0700

    update

--- src/API/weightData.ts
+++ src/API/weightData.ts
@@ -5,14 +5,13 @@ export const CreateNewweightData = async(newData:WeightData) => {
 	try {
 	console.log(newData)
 	const newDataCopy:any = {...newData}
-	newDataCopy.date = new Date(newDataCopy.date )
 	newDataCopy.weight = parseInt(newDataCopy.weight)
 	console.log(newDataCopy)
-	 axios.post('http://localhost:3001/weightTrackingData',{
+	const result = await axios.post('http://localhost:3001/weightTrackingData',{
 		date:newDataCopy.date,
 		weight: newDataCopy.weight
-
 	 })
+	console.log(result)
 	} catch (error) {
 	  console.error(error);
 	}
